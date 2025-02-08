778a234f3e37e4a50b4635a90b772ce544ea4ceb
adham <1adham0101797@gmail.com>
2025-02-08 14:10:43 +0200

    structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
+import { ErrorComponent } from './error/error.component';
+import { HeaderComponent } from './header/header.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { SearchComponent } from './search/search.component';
+import { WatchlistComponent } from './watchlist/watchlist.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavComponent,ErrorComponent,HeaderComponent,MovieDetailsComponent,SearchComponent,WatchlistComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
