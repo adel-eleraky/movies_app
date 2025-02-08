45afc3cec0316749d278916c41b43105884fe15c
el3raky <kameladel339@gmail.com>
2025-02-08 22:18:14 +0200

    fix: modified mis-spelling in api_key

--- src/app/services/movie-requests.service.ts
+++ src/app/services/movie-requests.service.ts
@@ -4,31 +4,31 @@ import { Observable } from 'rxjs';
 
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class MovieRequestsService {
 
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) { }
 
-   API_KEY:string='d6da8520522d81d3a36bfba09dfcbdc6';
-   path ="http://image.tmdb.org/t/p/w500"
+    API_KEY: string = 'd6da8520522d81d3a36bfba09dfcbdc6';
+    path = "http://image.tmdb.org/t/p/w500"
 
 
-  //  https://api.themoviedb.org/3/movie/now_playing
-  //  https://api.themoviedb.org/3/movie/${id}
-  getMovies():Observable<any>{
-    return this.http.get(`https://api.themoviedb.org/3/movie/now_playing`,{
-      params: {
-        API_KEY: this.API_KEY
-       
-      }
-    })
-  }
+    //  https://api.themoviedb.org/3/movie/now_playing
+    //  https://api.themoviedb.org/3/movie/${id}
+    getMovies(): Observable<any> {
+        return this.http.get(`https://api.themoviedb.org/3/movie/now_playing`, {
+            params: {
+                api_key: this.API_KEY
 
-  getMovieDetails(id: string) {
-    return this.http.get(`https://api.themoviedb.org/3/movie/${id}`)
-  }
-  
+            }
+        })
+    }
+
+    getMovieDetails(id: string) {
+        return this.http.get(`https://api.themoviedb.org/3/movie/${id}`)
+    }
+
 
 
 
