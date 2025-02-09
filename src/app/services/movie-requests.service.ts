1bb465f7694464e0d8379c2363ab7050abb66d8a
el3raky <kameladel339@gmail.com>
2025-02-09 21:56:56 +0200

    fix: modified the get movies func to accept lang and make movies as observable

--- src/app/services/movie-requests.service.ts
+++ src/app/services/movie-requests.service.ts
@@ -1,33 +1,40 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class MovieRequestsService {
 
-    constructor(private http: HttpClient) { }
+  private moviesSubject = new BehaviorSubject<any[]>([]);
+  movies$ = this.moviesSubject.asObservable();
 
-    API_KEY: string = 'd6da8520522d81d3a36bfba09dfcbdc6';
-    path = "http://image.tmdb.org/t/p/w500"
+  constructor(private http: HttpClient) {
+    this.getMovies();
+  }
+
+  API_KEY: string = 'd6da8520522d81d3a36bfba09dfcbdc6';
+  path = "http://image.tmdb.org/t/p/w500"
 
 
-    //  https://api.themoviedb.org/3/movie/now_playing
-    //  https://api.themoviedb.org/3/movie/${id}
-    getMovies(): Observable<any> {
-        return this.http.get(`https://api.themoviedb.org/3/movie/now_playing`, {
-            params: {
-                api_key: this.API_KEY
+  //  https://api.themoviedb.org/3/movie/now_playing
+  //  https://api.themoviedb.org/3/movie/${id}
+  getMovies(lang: string = "en"): void {
+    this.http.get(`https://api.themoviedb.org/3/movie/now_playing`, {
+      params: {
+        api_key: this.API_KEY,
+        language: lang
+      }
+    }).subscribe((response: any) => {
+      this.moviesSubject.next(response.results);
+    });
+  }
 
-            }
-        })
-    }
-
-    getMovieDetails(id: string) {
-        return this.http.get(`https://api.themoviedb.org/3/movie/${id}`)
-    }
+  getMovieDetails(id: string) {
+    return this.http.get(`https://api.themoviedb.org/3/movie/${id}`)
+  }
 
 
 
