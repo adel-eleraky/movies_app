fde0752fa1cccb4e85a7e11c44c3dfcaede4871e
el3raky <kameladel339@gmail.com>
2025-02-09 21:57:23 +0200

    feat: created handleLang function

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,21 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { WatchlistService } from '../services/watchlist.service';
+import { MovieRequestsService } from '../services/movie-requests.service';
 
 @Component({
   selector: 'app-nav',
-  imports: [RouterLink , RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
   watchlistCount = 0;
 
-  constructor(private watchlistService: WatchlistService) {}
+  constructor(private watchlistService: WatchlistService, private MovieRequestsService: MovieRequestsService) { }
 
   ngOnInit(): void {
     this.watchlistService.watchlistCount$.subscribe((count) => {
       this.watchlistCount = count;
     });
+  }
+
+  handleLang(lang: string) {
+
+    this.MovieRequestsService.getMovies(lang);
+
+    if (lang === 'ar') {
+      document.getElementById("movie-list")!.setAttribute('dir', 'rtl');
+    } else {
+      document.getElementById("movie-list")!.setAttribute('dir', 'ltr');
+    }
   }
 }
