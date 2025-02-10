3ed216ed7bb616e618b207f2b7b09a4033055544
ahmedrashad80 <ahmedmohamedrashad80@gmail.com>
2025-02-10 12:25:00 +0200

    edit language

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -7,27 +7,44 @@ import { MovieRequestsService } from '../services/movie-requests.service';
   selector: 'app-nav',
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrl: './nav.component.css',
 })
 export class NavComponent {
   watchlistCount = 0;
-
-  constructor(private watchlistService: WatchlistService, private MovieRequestsService: MovieRequestsService) { }
+  // toDo
+  currentPage: number = 1;
+  lang: string = 'en';
+  moviesData: any;
+  constructor(
+    private watchlistService: WatchlistService,
+    private MovieRequestsService: MovieRequestsService
+  ) {}
 
   ngOnInit(): void {
     this.watchlistService.watchlistCount$.subscribe((count) => {
       this.watchlistCount = count;
     });
+    this.MovieRequestsService.getCurrentPage().subscribe((page) => {
+      this.currentPage = page;
+    });
+    this.MovieRequestsService.getLanguage().subscribe((language) => {
+      this.lang = language;
+    });
   }
 
   handleLang(lang: string) {
-
-    this.MovieRequestsService.getMovies(lang);
+    this.MovieRequestsService.setLanguage(lang);
+    this.MovieRequestsService.getMovies(lang, this.currentPage).subscribe(
+      (movies) => {
+        this.moviesData = movies.results;
+        console.log(movies);
+      }
+    );
 
     if (lang === 'ar') {
-      document.getElementById("movie-list")!.setAttribute('dir', 'rtl');
+      document.getElementById('movie-list')!.setAttribute('dir', 'rtl');
     } else {
-      document.getElementById("movie-list")!.setAttribute('dir', 'ltr');
+      document.getElementById('movie-list')!.setAttribute('dir', 'ltr');
     }
   }
 }
