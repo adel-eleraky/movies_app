8f836f2666be6b99038f141f0789b03f439cf92c
ahmedrashad80 <ahmedmohamedrashad80@gmail.com>
2025-02-10 12:27:44 +0200

    handle shareable data

--- src/app/services/movie-requests.service.ts
+++ src/app/services/movie-requests.service.ts
@@ -2,42 +2,53 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MovieRequestsService {
-
-  private moviesSubject = new BehaviorSubject<any[]>([]);
-  movies$ = this.moviesSubject.asObservable();
+  private moviesSubject = new BehaviorSubject<any>([]);
+  // movies$ = this.moviesSubject.asObservable();
+  private currentPage = new BehaviorSubject<number>(1);
+  private language = new BehaviorSubject<string>('en');
 
   constructor(private http: HttpClient) {
-    this.getMovies();
+    // return this.getMovies();
   }
 
   API_KEY: string = 'd6da8520522d81d3a36bfba09dfcbdc6';
-  path = "http://image.tmdb.org/t/p/w500"
-
+  path = 'http://image.tmdb.org/t/p/w500';
 
   //  https://api.themoviedb.org/3/movie/now_playing
   //  https://api.themoviedb.org/3/movie/${id}
-  getMovies(lang: string = "en"): void {
-    this.http.get(`https://api.themoviedb.org/3/movie/now_playing`, {
-      params: {
-        api_key: this.API_KEY,
-        language: lang
+  getMovies(lang: string, page: number) {
+    return this.http.get<any>(
+      `https://api.themoviedb.org/3/movie/now_playing`,
+      {
+        params: {
+          api_key: this.API_KEY,
+          page: page.toString(),
+          language: lang,
+        },
       }
-    }).subscribe((response: any) => {
-      this.moviesSubject.next(response.results);
-    });
+    );
+    // .subscribe((response: any) => {
+    //   this.moviesSubject.next(response);
+    // });
   }
 
   getMovieDetails(id: string) {
-    return this.http.get(`https://api.themoviedb.org/3/movie/${id}`)
+    return this.http.get(`https://api.themoviedb.org/3/movie/${id}`);
   }
-
-
-
-
-
+  getCurrentPage() {
+    return this.currentPage.asObservable();
+  }
+  setCurrentPage(page: number) {
+    this.currentPage.next(page);
+  }
+  getLanguage() {
+    return this.language.asObservable();
+  }
+  setLanguage(lang: string) {
+    this.language.next(lang);
+  }
 }
