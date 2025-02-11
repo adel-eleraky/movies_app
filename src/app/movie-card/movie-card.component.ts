af470d30a098e9099ffd19e60ccc7dce30525272
ahmedrashad80 <ahmedmohamedrashad80@gmail.com>
2025-02-11 18:16:56 +0200

    spop propagation of watchlist icon

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, NgModule } from '@angular/core';
-import { RouterLink, RouterLinkActive ,Router} from '@angular/router';
+import { RouterLink, RouterLinkActive, Router } from '@angular/router';
 import { VotePipe } from '../pipes/vote.pipe';
 import { NgClass } from '@angular/common';
 import { WatchlistService } from '../services/watchlist.service';
@@ -8,33 +8,38 @@ import { CircularProgressComponent } from '../circular-progress/circular-progres
 
 @Component({
   selector: 'app-movie-card',
-  imports: [VotePipe , SlicePipe, CircularProgressComponent, RouterLink, RouterLinkActive],
+  imports: [
+    VotePipe,
+    SlicePipe,
+    CircularProgressComponent,
+    RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './movie-card.component.html',
-  styleUrl: './movie-card.component.css'
+  styleUrl: './movie-card.component.css',
 })
 export class MovieCardComponent {
+  @Input() movieData: any;
 
-    @Input() movieData: any
+  constructor(
+    private watchlistService: WatchlistService,
+    private router: Router
+  ) {}
 
-    constructor(
-      private watchlistService: WatchlistService,
-      private router: Router
-    ) {}
-
-    toggleWatchlist(movieData: any) {
-      if (this.watchlistService.isInWatchlist(movieData.id)) {
-        this.watchlistService.removeFromWatchlist(movieData.id);
-      } else {
-        this.watchlistService.addToWatchlist(movieData);
-      }
+  toggleWatchlist(movieData: any, event: MouseEvent) {
+    event.stopPropagation();
+    if (this.watchlistService.isInWatchlist(movieData.id)) {
+      this.watchlistService.removeFromWatchlist(movieData.id);
+    } else {
+      this.watchlistService.addToWatchlist(movieData);
     }
+  }
 
-    isInWatchlist(): boolean {
-      return this.watchlistService.isInWatchlist(this.movieData.id);
-    }
+  isInWatchlist(): boolean {
+    return this.watchlistService.isInWatchlist(this.movieData.id);
+  }
 
-
-    handleRedirectToDetails(id: any) {
-      this.router.navigate(['/moive-details', id]);
-    }
+  handleRedirectToDetails(id: any) {
+    this.router.navigate(['/moive-details', id]);
+  }
 }
