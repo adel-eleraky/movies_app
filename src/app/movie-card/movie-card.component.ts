3b5d923bb1d5118309595e03ba149e5b7093f4ba
el3raky <kameladel339@gmail.com>
2025-02-11 12:03:34 +0200

    refactor: import circle-progress

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -3,10 +3,11 @@ import { VotePipe } from '../pipes/vote.pipe';
 import { NgClass } from '@angular/common';
 import { WatchlistService } from '../services/watchlist.service';
 import { SlicePipe } from '@angular/common';
+import { CircularProgressComponent } from '../circular-progress/circular-progress.component';
 
 @Component({
   selector: 'app-movie-card',
-  imports: [VotePipe ,NgClass, SlicePipe],
+  imports: [VotePipe ,NgClass, SlicePipe, CircularProgressComponent],
   templateUrl: './movie-card.component.html',
   styleUrl: './movie-card.component.css'
 })
@@ -26,5 +27,10 @@ export class MovieCardComponent {
 
     isInWatchlist(): boolean {
       return this.watchlistService.isInWatchlist(this.movieData.id);
+    }
+
+    getVoteAverage(vote: number): number {
+      console.log(vote)
+      return Math.floor(vote * 10)
     }
 }
