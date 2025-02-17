bb95b899b456bb57a62f83dcca37d60ded0b6c58
ahmedrashad80 <ahmedmohamedrashad80@gmail.com>
2025-02-17 16:37:44 +0200

    fix:count page

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,60 +6,61 @@ import { FormsModule } from '@angular/forms';
 import { PaginationComponent } from '../pagination/pagination.component';
 
 @Component({
-    selector: 'app-header',
-    imports: [MovieCardComponent, FormsModule, PaginationComponent],
-    templateUrl: './header.component.html',
-    styleUrl: './header.component.css'
+  selector: 'app-header',
+  imports: [MovieCardComponent, FormsModule, PaginationComponent],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-    searchQuery: string = '';
+  searchQuery: string = '';
 
+  constructor(
+    private MovieRequestsService: MovieRequestsService,
+    private router: Router
+  ) {}
+  moviesData: any;
+  lang: string = 'en';
+  totalPages: number = 0;
+  currentPage: number = 1;
 
-    constructor(private MovieRequestsService: MovieRequestsService, private router: Router) { }
-    moviesData: any;
-    lang: string = 'en';
-    totalPages: number = 0;
-    currentPage: number = 1;
-
-
-    searchMovies(): void {
-        if (this.searchQuery.trim() !== '') {
-            this.router.navigate(['/search'], {
-                queryParams: { query: this.searchQuery },
-            });
-        }
+  searchMovies(): void {
+    if (this.searchQuery.trim() !== '') {
+      this.router.navigate(['/search'], {
+        queryParams: { query: this.searchQuery },
+      });
     }
+  }
 
-    ngOnInit() {
-        // this.MovieRequestsService.movies$.subscribe((movies) => {
-        //   this.moviesData = movies.results;
-        //   console.log(movies);
-        //   this.totalPages = movies.total_pages;
-        // });
-        this.MovieRequestsService.getLanguage().subscribe((language) => {
-            this.lang = language;
-            this.fetchMovies();
-        });
-        this.MovieRequestsService.getCurrentPage().subscribe((page) => {
-            this.currentPage = page;
+  ngOnInit() {
+    // this.MovieRequestsService.movies$.subscribe((movies) => {
+    //   this.moviesData = movies.results;
+    //   console.log(movies);
+    //   this.totalPages = movies.total_pages;
+    // });
+    this.MovieRequestsService.getLanguage().subscribe((language) => {
+      this.lang = language;
+      this.fetchMovies();
+    });
+    this.MovieRequestsService.getCurrentPage().subscribe((page) => {
+      this.currentPage = page;
 
-            this.fetchMovies();
-        });
-        // this.fetchMovies();
-    }
-    getCurrentPage(page: number) {
-        this.currentPage = page;
-        console.log(this.currentPage);
-        this.MovieRequestsService.setCurrentPage(page);
-        this.fetchMovies();
-    }
-    fetchMovies() {
-        this.MovieRequestsService.getMovies(this.lang, this.currentPage).subscribe(
-            (movies) => {
-                this.moviesData = movies.results;
-                console.log(movies);
-                this.totalPages = movies.total_pages;
-            }
-        );
-    }
+      this.fetchMovies();
+    });
+    // this.fetchMovies();
+  }
+  getCurrentPage(page: number) {
+    this.currentPage = page;
+    console.log(this.currentPage);
+    this.MovieRequestsService.setCurrentPage(page);
+    this.fetchMovies();
+  }
+  fetchMovies() {
+    this.MovieRequestsService.getMovies(this.lang, this.currentPage).subscribe(
+      (movies) => {
+        this.moviesData = movies.results;
+        console.log(movies);
+        this.totalPages = 500;
+      }
+    );
+  }
 }
